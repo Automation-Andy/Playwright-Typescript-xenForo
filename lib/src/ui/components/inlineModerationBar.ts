58d3264d1cc4af0c57aab50e6d41b6a637bd8d4f
Andy Gleed <andy@automationandy.net>
2024-09-26 20:15:17 +0100

    Fix thread delete moderation and added post moderation

--- lib/src/ui/components/inlineModerationBar.ts
+++ lib/src/ui/components/inlineModerationBar.ts
@@ -1,16 +1,27 @@
-import { Page } from '@playwright/test';
+import { expect, Page } from '@playwright/test';
 
 export class InlineModerationBar {
-  private readonly _container = this.page.locator('.inline-moderator-bar');
+  private readonly _container = this.page.locator('.inlineModBar');
   readonly _locators = {
+    lblTitle: this._container.locator('li').first(),
     chkSelectAll: this._container.getByText('Select all', { exact: true }),
-    cmbChooseActon: this._container.getByRole('combobox'),
+    cmbChooseActon: this._container.locator('select'),
     btnGo: this.page.getByRole('button', { name: 'Go' }),
   };
   constructor(private readonly page: Page) {}
 
-  async moderate(selectAll: boolean, action: string) {
+  async moderateThreads(selectAll: boolean, action: ModerateThreadsActions) {
+    await expect(this._locators.lblTitle).toHaveText('Threads (0)');
     await this.setSelectAll(selectAll);
+    await expect(this._locators.lblTitle).toHaveText(/Threads \([1-9]\d*\)/);
+    await this.selectAction(action);
+    await this.clickGo();
+  }
+
+  async moderatePosts(selectAll: boolean, action: ModeratePostsActions) {
+    await expect(this._locators.lblTitle).toHaveText('Posts (0)');
+    await this.setSelectAll(selectAll);
+    await expect(this._locators.lblTitle).toHaveText(/Posts \([1-9]\d*\)/);
     await this.selectAction(action);
     await this.clickGo();
   }
@@ -19,11 +30,35 @@ export class InlineModerationBar {
     await this._locators.chkSelectAll.setChecked(checked);
   }
 
-  async selectAction(action: string) {
-    await this._locators.cmbChooseActon.selectOption({ label: action });
+  async selectAction(action: ModerateThreadsActions | ModeratePostsActions) {
+    await this._locators.cmbChooseActon.selectOption(action.valueOf());
   }
 
   async clickGo() {
     await this._locators.btnGo.click();
   }
+}
+
+export enum ModerateThreadsActions {
+  DeleteThreads = 'delete',
+  UndeleteThreads = 'undelete',
+  ApproveThreads = 'approve',
+  UnapproveThreads = 'unapprove',
+  StickThreads = 'stick',
+  UnstickThreads = 'unstick',
+  LockThreads = 'lock',
+  UnlockThreads = 'unlock',
+  MoveThreads = 'move',
+  ApplyPrefix = 'apply_prefix',
+  ChangeThreadType = 'change_type',
+}
+
+export enum ModeratePostsActions {
+  DeletePosts = 'delete',
+  UndeletePosts = 'undelete',
+  ApprovePosts = 'approve',
+  UnapprovePosts = 'unapprove',
+  MovePosts = 'move',
+  CopyPosts = 'copy',
+  MergePosts = 'merge',
 }
